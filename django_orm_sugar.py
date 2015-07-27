c98516c8dc0f68835f4d88af682c92a4ba90ba50
Alexey Zankevich <zancudero@gmail.com>
2015-07-27 08:21:34 +0300

    Added util methods to sugar class

--- django_orm_sugar.py
+++ django_orm_sugar.py
@@ -1,27 +1,66 @@
-from django.db.models import Q as DjangoQ
+from django.db.models import Q
 
 __author__ = 'Alexey Zankevich'
 
 
-class Q(object):
+class Sugar(object):
     """
-    >>> q = Q()
-    >>> q.username.get_query_param()
+    S - Django ORM Sugar
+
+    >>> s = Sugar()
+    >>> s.username.get_query_param()
     'username'
 
-    >>> Q().user.username.get_query_param()
+    >>> Sugar().user.username.get_query_param()
     'user__username'
 
+    Typical usage:
+    >>> Sugar().user.username == 'John Smith'
+    <Q: (AND: ('user__username__exact', 'John Smith'))>
+
     """
-    def __init__(self, name='', parent=None):
-        self.__parent = parent
-        self.__name = name
+    def __init__(self, *args, **kwargs):
+        self.__parent = kwargs.pop('__parent', None)
+        self.__name = kwargs.pop('__name', '')
 
     def __getattr__(self, item):
-        return Q(item, self)
+        return Sugar(__name=item, __parent=self)
 
-    def __unicode__(self):
-        return "Q('{}')".format(self.__name)
+    def __eq__(self, value):
+        """
+        >>> Sugar().user.username == 'John Smith'
+        <Q: (AND: ('user__username__exact', 'John Smith'))>
+        """
+        return self.exact(value)
+
+    def __gt__(self, value):
+        """
+        >>> Sugar().user.age > 7
+        <Q: (AND: ('user__age__gt', 7))>
+        """
+        return Q(**{'{}__gt'.format(self.get_query_param()): value})
+
+    def __ge__(self, value):
+        """
+        >>> Sugar().user.age >= 7
+        <Q: (AND: ('user__age__gte', 7))>
+        """
+        return Q(**{'{}__gte'.format(self.get_query_param()): value})
+
+    def in_list(self, lst):
+        return Q(**{'{}__in'.format(self.get_query_param()): lst})
+
+    def iexact(self, ):
+        pass
+
+    def exact(self, value):
+        return Q(**{'{}__exact'.format(self.get_query_param()): value})
+
+    def contains(self, s):
+        return Q(**{'{}__contains'.format(self.get_query_param()): s})
+
+    def icontains(self, s):
+        return Q(**{'{}__icontains'.format(self.get_query_param()): s})
 
     def get_query_param(self):
         if self.__parent:
