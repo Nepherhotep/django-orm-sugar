e2f54f739eb5a7cb4ebd9b8e52bb370b7fb73bfb
Alexey Zankevich <zancudero@gmail.com>
2015-07-28 13:44:51 +0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Alexey Zankevich'
+
+
+setup(
+    name="django_orm_sugar",
+    version="0.1",
+    packages=find_packages(),
+    author="Alexey Zankevich",
+    author_email="alex.zankevich@gmail.com",
+    description="Django ORM sugar library to simplify querying",
+    license="MIT",
+)
+
+
+
