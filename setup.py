5a748321d8b7e1fc4d7acd036a02253a6cd9f8e8
Alexey Zankevich <zancudero@gmail.com>
2015-08-16 12:04:37 +0300

    Added classifiers

--- setup.py
+++ setup.py
@@ -5,13 +5,22 @@ __author__ = 'Alexey Zankevich'
 
 setup(
     name="django-orm-sugar",
-    version="0.1.4",
+    version="0.2.1",
     py_modules=['django_orm_sugar'],
     author="Alexey Zankevich",
     author_email="alex.zankevich@gmail.com",
     description="Django ORM sugar library to simplify querying",
     keywords=['Django', 'ORM', 'util', 'sugar'],
     license="MIT",
+    classifiers=["Framework :: Django :: 1.8",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.0",
+                 "Programming Language :: Python :: 3.1",
+                 "Programming Language :: Python :: 3.2",
+                 "Programming Language :: Python :: 3.3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5"
+    ]
 )
 
 
