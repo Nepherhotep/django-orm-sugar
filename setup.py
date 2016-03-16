821266e9f282db05a19bc5a235002dd96b9ca63d
Alexey Zankevich <zancudero@gmail.com>
2016-03-16 15:55:44 +0300

    updated django orm sugar version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'Alexey Zankevich'
 
 setup(
     name="django-orm-sugar",
-    version="0.3.1",
+    version="0.3.2",
     py_modules=['django_orm_sugar'],
     author="Alexey Zankevich",
     author_email="alex.zankevich@gmail.com",
